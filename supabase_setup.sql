e74e0f9ee2ce5a6858ba389be685735b7ed5cdbd
patricklgould <patricklgould@gmail.com>
2026-06-23 20:02:03 -0400

    Delete supabase_setup.sql

--- supabase_setup.sql
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://hsoypayfbosdiqyicrqx.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_tqd7FQNGPmuhgdhAqsnKgw_8roeD_n-';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
